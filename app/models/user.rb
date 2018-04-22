390e84ea6c803efbbbc1076ecc22d3f36ffa32b4
huyennnnnn <nguyenhoang04111996@gmail.com>
2018-04-22 08:51:16 +0000

    finish chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :ideas
+  include Gravtastic
+  gravtastic
 end
